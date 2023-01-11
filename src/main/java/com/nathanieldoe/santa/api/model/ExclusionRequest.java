e9e7b4da48d6cd2cc8f3de98a17b8bcc8f35cad6
Nathan Doe <ndoe@interactions.com>
2023-01-11 08:59:58 -0500

    Fixing NPE when year not provided

--- src/main/java/com/nathanieldoe/santa/api/model/ExclusionRequest.java
+++ src/main/java/com/nathanieldoe/santa/api/model/ExclusionRequest.java
@@ -2,7 +2,7 @@ package com.nathanieldoe.santa.api.model;
 
 import com.nathanieldoe.santa.model.Person;
 
-public record ExclusionRequest(Person exclusion, Integer year) {
+public record ExclusionRequest(Person exclusion, int year) {
 
     @Override
     public String toString() {
