a3e0b7c99155398341b460fe30f881b60b66d96c
Nathan Doe <ndoe@interactions.com>
2023-04-27 14:59:29 -0400

    Updating full context load unit test leveraging test containers

--- src/test/java/com/nathanieldoe/santa/SecretSantaApiApplicationTests.java
+++ src/test/java/com/nathanieldoe/santa/SecretSantaApiApplicationTests.java
@@ -9,15 +9,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Sanity checks that the server context will start
  */
-@SpringBootTest
-@ActiveProfiles("test")
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SecretSantaApiApplicationTests {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SecretSantaApiApplicationTests.class);
@@ -30,6 +34,18 @@ class SecretSantaApiApplicationTests {
 
 	@Autowired
 	private PersonApiController personApiController;
+
+	@Container
+	public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+			.withPassword("inmemory")
+			.withUsername("inmemory");
+
+	@DynamicPropertySource
+	static void postgresqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+	}
 
 	@Test
 	void contextLoads() {
